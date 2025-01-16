5d719902eb339ada03506387eb446ef65a2cb6b8
SUSHANK-ops <sushanklamichhane12@gmail.com>
2025-01-16 18:28:48 +0545

    ads

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Head from "next/head"; // Import Head for adding meta and script tags
+import Head from "next/head"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
